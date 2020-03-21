753bb19f2e8b306bcf82495f528550c2e2b64656
张丘洋 <cs.qyzhang@qq.com>
2020-03-21 11:54:48 +0800

    add setup.sh

---
+++ script/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt install build-essential cmake libfuse3-3 libfuse3-dev python3 ipython3 python3-matplotlib bison flex texlive texlive-xetex
+cd ..
+mkdir build
+cd build
+cmake ..
+make
+mkdir mount
+mkdir datadir
+mkdir metadir
+cd ../../
+wget https://github.com/filebench/filebench/releases/download/1.5-alpha3/filebench-1.5-alpha3.tar.gz
+tar -xzvf filebench-1.5-alpha3.tar.gz
+cd filebench-1.5-alpha3
+./configure
+make
+echo 0 > /proc/sys/kernel/randomize_va_space
+cd ../KTableFS/script
