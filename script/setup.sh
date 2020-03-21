4842b7c54d54d0a56f3b0cf0055ef602417c6eae
张丘洋 <cs.qyzhang@qq.com>
2020-03-21 14:43:20 +0800

    fix CMakeLists

--- script/setup.sh
+++ script/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install build-essential cmake libfuse3-3 libfuse3-dev python3 ipython3 python3-matplotlib bison flex texlive texlive-xetex
+apt install build-essential cmake libfuse3-3 libfuse3-dev python3 ipython3 python3-matplotlib bison flex texlive texlive-xetex
 cd ..
 mkdir build
 cd build
